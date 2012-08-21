b8e05ce2f4ff1abfe17ba9dcd76fe2f22a4039e5
kyleandrus <kyleandrus@gmail.com>
2012-08-21 23:28:26 +0000

    Added the ability to write to a log file, I set the program to run for 10 minutes then rescan. This way you can leave the script running over night and it rescans the notification files as they're created to try and find duplicates.

--- NotificationScanner.py
+++ NotificationScanner.py
@@ -10,6 +10,9 @@ notified and the duplicate is shown
 '''
 import os 
 import re 
+import sys
+import logging
+import time
 
 def trans_id_match(not_log_dir):
     '''This method takes the location of notification files as an argument,
@@ -17,6 +20,14 @@ def trans_id_match(not_log_dir):
     Returns a dictionary containing the trans-id as the key, the number of duplicates 
     and the location of the duplicate'''
     
+    #Set logger variables etc...
+    logger = logging.getLogger('NotificationScanner')
+    hdlr = logging.FileHandler(os.path.join(not_log_dir, "Scanner_Results.log"))
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(logging.DEBUG)
+
     #Populate the not_files list with the absolute paths of all the notification files
     #in the directory
     not_files = []
@@ -33,7 +44,7 @@ def trans_id_match(not_log_dir):
         file_data = FILE.read()
         temp_id_matches = re.findall(r'\S+TransID:([0-9A-Fa-f]+-[0-9A-Fa-f]+-[0-9A-Fa-f]+-[0-9A-Fa-f]+-[0-9A-Fa-f]+)', file_data)
         
-                #Assign the notification file that the t-id comes from to the trans_ids dict for matching later if a duplicate is found
+        #Assign the notification file that the t-id comes from to the trans_ids dict for matching later if a duplicate is found
         for match in temp_id_matches:
             if not trans_ids.has_key(match):
                 trans_ids[match] = not_file
@@ -47,15 +58,52 @@ def trans_id_match(not_log_dir):
     #If the log generator is functioning properly, you should see nothing from the code below
     duplicate_dict = {}
     print "Transaction IDS:"
-    count = 0
+    count = 0   
     for t_id in trans_ids:
         if not 'duplicate' in t_id and duplicate_count[t_id] > 2:
-            print "%d duplicate(s) for %s, located in %s" %(duplicate_count[t_id] -2, t_id, trans_ids[t_id])
+            duplicate_string = "%d duplicate(s) for %s, located in %s" %(duplicate_count[t_id] -2, t_id, trans_ids[t_id])
+            print duplicate_string
+            logger.error(duplicate_string)
             duplicate_dict[t_id] = [duplicate_count[t_id], trans_ids[t_id]]
             count +=1
     if count == 0:
-        print "No Duplicate ID's found. Success!"
+        success_string = "No Duplicate ID(s) found, Success!"
+        print success_string
+        logger.info(success_string)
     del count
+    FILE.close()
     return duplicate_dict
             
-trans_id_match(r'C:\Log\Processor-KylesPyMatchTest\Notification')
+#trans_id_match(r'C:\Log\Processor-KylesPyMatchTest\Notification')
+
+
+def main():
+    '''This function is used to call the rest of the methods from the command line'''
+        
+    # Make a list of command line arguments, omitting the [0] element which is the script itself.
+    args = sys.argv[1:]
+    
+    #If no argruments are given, print proper usage and call the search method
+    if not args:
+        print "\nProper usage: \n\t[NotificationScanner.py \"c:\Log\AppName\Notifications\"]"
+    
+    #Default values passed to the method when called through a command line argument
+    not_log_dir = ''
+    if len(args) == 0:
+        print "\n\nYou must use a directory to proceed!"
+    elif args[0] != '':
+        not_log_dir = args[0]
+        
+        while True:
+            print "Press ctrl+c to stop scanning Notification files"
+            trans_id_match(not_log_dir)
+            time.sleep(600)
+        del args[0:0]
+        
+##uncomment to run the main method from the console        
+if __name__ == "__main__":
+    '''If the scripts initiates itself, run the main method
+    this prevent the main from being called if this module is 
+    imported into another script'''
+    main()
+
